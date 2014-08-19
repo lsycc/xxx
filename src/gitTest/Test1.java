75f9509b5676067afb1bfb32050bca277643c4f0
lusongyang <sh21134@sina.com>
2014-08-19 17:29:20 +0800

    add

--- src/gitTest/Test1.java
+++ src/gitTest/Test1.java
@@ -5,6 +5,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("testBranch");
 		System.out.println("== add");
+		System.out.println("---");System.out.println("testBranch");
+		System.out.println("== add");
+		System.out.println("---");
+		System.out.println("testBranch");
+		System.out.println("== add");
 		System.out.println("---");
 	}
 
