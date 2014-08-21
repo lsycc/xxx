f44f00cfc3d2874e5ab776646a655392cd35eb39
lusongyang <sh21134@sina.com>
2014-08-21 13:52:37 +0800

    del-w2

--- src/gitTest/A1.java
+++ src/gitTest/A1.java
@@ -3,7 +3,5 @@ package gitTest;
 public class A1 {
 public static void main(String[] args) {
 	System.out.println("A1-W-ORIGINMASTER");
-	System.out.println("1111111111");
-	System.out.println("222222222");
 }
 }
