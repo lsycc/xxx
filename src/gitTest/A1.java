5e41d71124411e2b50a2628f2ca13ea78ff2379d
lusongyang <sh21134@sina.com>
2014-08-21 13:38:31 +0800

    w2-1

--- src/gitTest/A1.java
+++ src/gitTest/A1.java
@@ -3,8 +3,5 @@ package gitTest;
 public class A1 {
 public static void main(String[] args) {
 	System.out.println("A1-W-ORIGINMASTER");
-	System.out.println("1111111111");
-	System.out.println("222222222");
-	System.out.println("13:15");
 }
 }
