84a4b989f7c5b29249294e2ef662d294813b987e
lsy <sh21134@sina.com>
2014-11-07 14:00:15 +0800

    xxx

--- src/gitTest/A1.java
+++ src/gitTest/A1.java
@@ -1,8 +1,10 @@
 package gitTest;
 
 public class A1 {
-public static void main(String[] args) {
-	System.out.println("A1-W-TER");System.out.println();
-	System.out.println("====");
-}
+	public static void main(String[] args) {
+		int f = 10;
+		int t = 3;
+		int t1 = 3;
+		System.out.println(f * 100 / t /100f);
+	}
 }
