3efc6367b801e276d38955309fc89da19c53d046
lsy <sh21134@sina.com>
2014-11-10 10:24:48 +0800

    add branch bb

--- src/gitTest/A1.java
+++ src/gitTest/A1.java
@@ -2,9 +2,6 @@ package gitTest;
 
 public class A1 {
 	public static void main(String[] args) {
-		int f = 10;
-		int t = 3;
-		int t1 = 3;
-		System.out.println(f * 100 / t /100f);
+		System.out.println("add bb branch");
 	}
 }
