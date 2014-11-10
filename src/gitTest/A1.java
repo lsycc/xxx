6e6279ce05f38d9d21ca6e0f8618cc0a030d332e
lsy <sh21134@sina.com>
2014-11-10 11:08:58 +0800

    add  3 brnachs

--- src/gitTest/A1.java
+++ src/gitTest/A1.java
@@ -3,5 +3,7 @@ package gitTest;
 public class A1 {
 	public static void main(String[] args) {
 		System.out.println("add bb branch");
+		System.out.println("add bb branch");
+		System.out.println("add bb branch");
 	}
 }
