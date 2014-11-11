ca13de9c340451748ec584aff77344cb620364ba
lsy <sh21134@sina.com>
2014-11-11 11:38:50 +0800

    ===============

--- src/gitTest/A1.java
+++ src/gitTest/A1.java
@@ -2,7 +2,6 @@ package gitTest;
 
 public class A1 {
 	public static void main(String[] args) {
-		System.out.println("add bb branch");
-		System.out.println("add bb branch");
+		System.out.println("============================");
 	}
 }
